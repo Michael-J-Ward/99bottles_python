ba6fe8e38b405655d89cd0eaa83b3a0b1ba1ee95
Michael J Ward <ward.michael.j@gmail.com>
2017-04-27 08:39:46 -0500

    Chapter 3

--- bottles.py
+++ bottles.py
@@ -6,6 +6,9 @@ class Bottles:
     def verses(self, start, end):
         return "\n".join(self.verse(i) for i in range(start, end - 1, -1))
 
+    def container(self, number):
+      return 'bottles' if number > 1 else 'bottle'
+
     def verse(self, number):
         verse_0 = (
             "No more bottles of beer on the wall, " +
@@ -14,27 +17,21 @@ class Bottles:
             "99 bottles of beer on the wall.\n"
         )
         verse_1 = (
-           "1 bottle of beer on the wall, " +
-           "1 bottle of beer.\n" +
-           "Take it down and pass it around, " +
-           "no more bottles of beer on the wall.\n"
-        )
-        verse_2 = (
-           "2 bottles of beer on the wall, " +
-           "2 bottles of beer.\n" +
-           "Take one down and pass it around, " +
-           "1 bottle of beer on the wall.\n"
+            "1 bottle of beer on the wall, " +
+            "1 bottle of beer.\n" +
+            "Take it down and pass it around, " +
+            "no more bottles of beer on the wall.\n"
         )
         default = (
-           f"{number} bottles of beer on the wall, " +
-           f"{number} bottles of beer.\n" +
-           "Take one down and pass it around, " +
-           f"{number-1} bottles of beer on the wall.\n"
+            f"{number} {self.container(number)} of beer on the wall, " +
+            f"{number} {self.container(number)} of beer.\n" +
+            "Take one down and pass it around, " +
+            f"{number-1} {self.container(number-1)} of beer on the wall.\n"
         )
+
         CASES = {
             0: verse_0,
             1: verse_1,
-            2: verse_2,
         }
         return CASES.get(number, default)
 
